9c795abd362f5f9275a5f4964999a14d4dd33e18
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-12-03 13:01:12 +0900

    fix build

--- mrbgem.rake
+++ mrbgem.rake
@@ -2,4 +2,7 @@ MRuby::Gem::Specification.new("mruby-secp256k1") do |spec|
   spec.license = "Unlicensed"
   spec.author  = "Nervos Core Dev <dev@nervos.org>"
   spec.summary = "secp256k1(with Nervos' patches) binding for mruby"
+
+  spec.cc.flags += [`pkg-config --static --cflags libsecp256k1`.strip]
+  spec.linker.flags += [`pkg-config --static --libs-only-L libsecp256k1`.strip]
 end
--- src/secp256k1.c
+++ src/secp256k1.c
@@ -8,8 +8,9 @@ extern void custom_abort();
 extern int custom_print_err(const char * arg, ...);
 #endif
 
-#include <secp256k1_static.h>
-#include <secp256k1.c>
+//#include <secp256k1_static.h>
+//#include <secp256k1.c>
+#include <secp256k1.h>
 
 #ifdef SECP256K1_DISABLE_SIGNING
 #ifdef SECP256K1_DISABLE_VERIFYING
@@ -30,21 +31,17 @@ secp256k1_mrb_pubkey(mrb_state *mrb, mrb_value obj)
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secret key length is not 32 bytes!");
   }
 
-  secp256k1_context context;
-  int ret = secp256k1_context_initialize(&context, SECP256K1_CONTEXT_SIGN);
-  if (ret == 0) {
-    mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 initialization failure!");
-  }
+  secp256k1_context *context = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
 
   secp256k1_pubkey pubkey;
-  ret = secp256k1_ec_pubkey_create(&context, &pubkey, secretkey_buf);
+  int ret = secp256k1_ec_pubkey_create(context, &pubkey, secretkey_buf);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 pubkey creation failure!");
   }
 
   unsigned char buf[256];
   size_t len = 256;
-  ret = secp256k1_ec_pubkey_serialize(&context, buf, &len, &pubkey, SECP256K1_EC_COMPRESSED);
+  ret = secp256k1_ec_pubkey_serialize(context, buf, &len, &pubkey, SECP256K1_EC_COMPRESSED);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 pubkey serializing failure!");
   }
@@ -68,14 +65,10 @@ secp256k1_mrb_sign(mrb_state *mrb, mrb_value obj)
     mrb_raise(mrb, E_ARGUMENT_ERROR, "message length is not 32 bytes!");
   }
 
-  secp256k1_context context;
-  int ret = secp256k1_context_initialize(&context, SECP256K1_CONTEXT_SIGN);
-  if (ret == 0) {
-    mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 initialization failure!");
-  }
+  secp256k1_context *context = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
 
   secp256k1_ecdsa_signature signature;
-  ret = secp256k1_ecdsa_sign(&context, &signature, message_buf, secretkey_buf,
+  int ret = secp256k1_ecdsa_sign(context, &signature, message_buf, secretkey_buf,
                              NULL, NULL);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 signing failure!");
@@ -83,7 +76,7 @@ secp256k1_mrb_sign(mrb_state *mrb, mrb_value obj)
 
   unsigned char buf[256];
   size_t len = 256;
-  ret = secp256k1_ecdsa_signature_serialize_der(&context, buf, &len, &signature);
+  ret = secp256k1_ecdsa_signature_serialize_der(context, buf, &len, &signature);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 signature_der serializing failure!");
   }
@@ -107,27 +100,23 @@ secp256k1_mrb_verify(mrb_state *mrb, mrb_value obj)
     mrb_raise(mrb, E_ARGUMENT_ERROR, "message length is not 32 bytes!");
   }
 
-  secp256k1_context context;
-  int ret = secp256k1_context_initialize(&context, SECP256K1_CONTEXT_VERIFY);
-  if (ret == 0) {
-    mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 initialization failure!");
-  }
+  secp256k1_context *context = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
 
   secp256k1_pubkey pubkey;
-  ret = secp256k1_ec_pubkey_parse(&context, &pubkey, pubkey_buf, pubkey_len);
+  int ret = secp256k1_ec_pubkey_parse(context, &pubkey, pubkey_buf, pubkey_len);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 pubkey parsing failure!");
   }
 
   secp256k1_ecdsa_signature signature;
-  ret = secp256k1_ecdsa_signature_parse_der(&context, &signature, signature_buf,
+  ret = secp256k1_ecdsa_signature_parse_der(context, &signature, signature_buf,
                                             signature_len);
   if (ret == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "secp256k1 signature_der parsing failure!");
   }
 
-  ret = secp256k1_ecdsa_verify(&context, &signature, message_buf, &pubkey);
-  secp256k1_context_deinitialize(&context);
+  ret = secp256k1_ecdsa_verify(context, &signature, message_buf, &pubkey);
+  secp256k1_context_destroy(context);
 
   if (ret == 1) {
     return mrb_true_value();
